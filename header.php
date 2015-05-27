7215ff340fb9fa5b560193d972cf861ec6e2d670
KatarinaH <katarina.hansson11@gmail.com>
2015-05-27 11:39:19 +0200

    favicon

--- header.php
+++ header.php
@@ -10,6 +10,7 @@ session_start(); //startar session
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
 </head>
 
 <body>
