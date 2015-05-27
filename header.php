3ec4c79455ce67a02afb347b628e689fb59692a5
KatarinaH <katarina.hansson11@gmail.com>
2015-05-27 12:20:55 +0200

    viewport awesomness

--- header.php
+++ header.php
@@ -7,6 +7,7 @@ session_start(); //startar session
 <head>
 	<title>PinYourPlace</title>
 	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width; initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'>
